cf5a708ee79f29b3c6ff926ae12b65f05ea1d6a9
jaqwe2301 <jaqwe2301@naver.com>
2023-12-12 21:14:14 +0900

    [FEAT] 지정 스타일 tailwind에 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "rodu-medium": "#f9911e",
+        "rodu-black": "#212121",
+        "rodu-grey": "#777777",
+      },
+      fontSize: {
+        body2: "14px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        ".button": {
+          fontSize: "14px",
+          fontWeight: "500",
+        },
+        ".body1": {
+          fontSize: "18px",
+        },
+        ".body2": {
+          fontSize: "14px",
+        },
+      };
+
+      addUtilities(newUtilities);
+    }),
+  ],
 };
