778331ad520827a8835d4057ac147194ecb30334
jaqwe2301 <jaqwe2301@naver.com>
2023-12-17 15:58:48 +0900

    Feat: jwt and jwt-decode

---
+++ src/utils/decode.ts
@@ -0,0 +1,11 @@
+import { jwtDecode, JwtPayload } from "jwt-decode";
+
+interface CustomJwtPayload extends JwtPayload {
+  roles: Array<{ authority: string }>;
+}
+
+export function handleAdminCheck() {
+  const token = localStorage.getItem("accessToken");
+  const decoded = token ? (jwtDecode(token) as CustomJwtPayload) : undefined;
+  return decoded && decoded.roles[0].authority === "ROLE_ADMIN";
+}
