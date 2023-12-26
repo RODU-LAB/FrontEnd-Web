4c77a4b61d9f4714b19e979d696e56f4a70a3707
jaqwe2301 <jaqwe2301@naver.com>
2023-12-26 21:27:14 +0900

    Feat: Add Modal

---
+++ src/components/modal/PwInputModal.tsx
@@ -0,0 +1,62 @@
+import Modal from "react-modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
+interface PwInputModalTypes {
+  isOpen: boolean;
+  enterPress: () => void;
+  onInput: (value: string) => void;
+  onClickXmark: () => void;
+}
+
+export function PwInputModal({
+  isOpen,
+  enterPress,
+  onInput,
+  onClickXmark,
+}: PwInputModalTypes) {
+  const handleEnterPress = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      enterPress();
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    onInput(target.value);
+  };
+
+  return (
+    <Modal isOpen={isOpen} className="Posts-modal">
+      <>
+        <div className="Posts-modal-container">
+          <span className="Posts-modal-title flex justify-between items-center px-4">
+            <div className="w-[15px]" />
+            <p>게시글 비밀번호를 입력해주세요</p>
+            <FontAwesomeIcon
+              icon={faXmark}
+              size="lg"
+              className="cursor-pointer"
+              onClick={onClickXmark}
+            />
+          </span>
+          <div className="flex w-full Posts-modal-form">
+            <input
+              onInput={handleInputChange}
+              placeholder="비밀번호를 입력해주세요"
+              className="Posts-modal-input"
+              type="password"
+              onKeyDown={handleEnterPress}
+            />
+            <button
+              className="Posts-modal-button transition-colors"
+              onClick={enterPress}
+            >
+              확인
+            </button>
+          </div>
+        </div>
+      </>
+    </Modal>
+  );
+}
