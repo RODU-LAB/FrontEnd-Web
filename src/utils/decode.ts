99e8ec02f6fbe2441a1fc6034eaa22209751978e
jaqwe2301 <jaqwe2301@naver.com>
2024-01-18 21:57:18 +0900

    Fix: config.ts - not Found state file

--- src/utils/decode.ts
+++ src/utils/decode.ts
@@ -1,13 +1,11 @@
 import { jwtDecode, JwtPayload } from "jwt-decode";
-import { useRecoilValue } from "recoil";
-import { accessTokenState } from "./state";
 
 interface CustomJwtPayload extends JwtPayload {
   roles: Array<{ authority: string }>;
 }
 
 export function useAdminCheck() {
-  const token = useRecoilValue(accessTokenState);
+  const token = localStorage.getItem("accessToken");
   const decoded = token ? (jwtDecode(token) as CustomJwtPayload) : undefined;
   return decoded && decoded.roles[0].authority === "ROLE_ADMIN";
 }
